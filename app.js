498e669dbbf62853a97e55ae7983c22078861672
cody <codyjeromegreen@gmail.com>
2017-11-04 22:44:57 -0700

    done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var lastIndex = [];
 var totalClicks = [];
 var viewResultsButton = document.createElement('button');
 var canvas = document.getElementById('myChart');
+var clear = document.getElementById('clearStorage');
 Chart.defaults.global.defaultFontColor = '#fff';
 
 //variable to end the survey after 25 clicks
@@ -70,6 +71,10 @@ function localData() {
     createItem();
   }
 };
+clear.addEventListener('click', function() {
+  console.log('click it!');
+  localStorage.clear();
+});
 //function to end the survey
 function surveyEnd() {
   viewResultsButton.textContent = 'View Results';
@@ -150,50 +155,49 @@ var data = {
     {
       label: 'Products Chosen',
       backgroundColor: [
-        'rgba(244, 255, 35, 0.2)',
-        'rgba(60, 180, 75, 0.2)',
-        'rgba(255, 225, 25, 0.2)',
-        'rgba(0, 130, 200, 0.2)',
-        'rgba(245, 130, 48, 0.2)',
-        'rgba(145, 30, 180, 0.2)',
-        'rgba(70, 240, 240, 0.2)',
-        'rgba(240, 50, 230, 0.2)',
-        'rgba(210, 245, 60, 0.2)',
-        'rgba(250, 190, 190, 0.2)',
-        'rgba(0, 128, 128, 0.2)',
-        'rgba(230, 190, 255, 0.2)',
-        'rgba(170, 110, 40, 0.2)',
-        'rgba(255, 250, 200, 0.2)',
-        'rgba(128, 0, 0, 0.2)',
-        'rgba(170, 255, 195, 0.2)',
-        'rgba(128, 128, 0, 0.2)',
-        'rgba(255, 215, 180, 0.2)',
-        'rgba(0, 0, 128, 0.2)',
-        'rgba(128, 128, 128, 0.2)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)'
 
       ],
       borderColor: [
-        'rgba(244, 255, 35,1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)',
-        'rgba(244, 255, 35, 1)'
-
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)',
+        'rgba(105, 112, 213, 1)',
+        'rgba(86, 215, 152, 1)'
       ],
       borderWidth: 1,
       data: totalClicks,
@@ -204,7 +208,7 @@ var data = {
 //Function to Draw Chart
 function drawChart() {
   var ctx = canvas.getContext('2d');
-  var myBarChart = new Chart(ctx, {scaleFontColor:'yellow',
+  var myBarChart = new Chart(ctx, {
     type: 'bar',
     data: data,
   });
